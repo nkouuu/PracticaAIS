7a9d204a4c1adc80daab1bf41c89d92d20128fbc
Niko <nkouuu@hotmail.com>
2018-04-05 11:43:17 +0200

    Inicio

---
+++ PracticaAIS1/BuscaminasGit/src/buscaminasgit/BuscaminasGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminasgit;
+
+/**
+ *
+ * @author nicolaealexe
+ */
+public class BuscaminasGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
