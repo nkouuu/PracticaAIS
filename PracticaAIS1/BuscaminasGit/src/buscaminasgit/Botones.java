008a89e1aa316885b2d036c42c4aec33e898f857
Diego Méndez <diegomen15@gmail.com>
2018-04-12 13:23:51 +0200

    .

---
+++ PracticaAIS1/BuscaminasGit/src/buscaminasgit/Botones.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminasgit;
+
+import java.awt.Color;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Diegomendez1997
+ */
+public class Botones implements Serializable{
+    String text;
+    boolean estado;
+    //Color color;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    /*public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }*/
+    
+    
+}
--- PracticaAIS1/BuscaminasGit/src/buscaminasgit/BuscaminasGit.java
+++ PracticaAIS1/BuscaminasGit/src/buscaminasgit/BuscaminasGit.java
@@ -102,17 +102,7 @@ public class BuscaminasGit extends JFrame implements ActionListener, MouseListen
             } else {
                 JOptionPane.showMessageDialog(this, "Fichero NO guardado", "Guardar Fichero", JOptionPane.ERROR_MESSAGE);
             }
-        }
-
-                boolean resultadoOK = this.hacerBackUp(selectorFichero.getSelectedFile().getAbsolutePath());
-                if (resultadoOK) {
-                    JOptionPane.showMessageDialog(this, "Fichero guardado correctamente", "Guardar Fichero", JOptionPane.INFORMATION_MESSAGE);
-                } else {
-                    JOptionPane.showMessageDialog(this, "Fichero NO guardado", "Guardar Fichero", JOptionPane.ERROR_MESSAGE);
-                }
-            
-                
-            
+        }  
         });
         menu.add(mi2);
         mi3 = new JMenuItem("Cargar partida");
@@ -127,7 +117,6 @@ public class BuscaminasGit extends JFrame implements ActionListener, MouseListen
                 boolean resultadoOK = this.restaurarBackUp(selectorFichero.getSelectedFile().getAbsolutePath());
                 if (resultadoOK) {
                     this.setVisible(false);
-                    buscaminas.setVisible(true);
                     JOptionPane.showMessageDialog(this, "Fichero cargado correctamente", "Cargar Fichero", JOptionPane.INFORMATION_MESSAGE);
                 } else {
                     JOptionPane.showMessageDialog(this, "Fichero NO cargado", "Cargar Fichero", JOptionPane.ERROR_MESSAGE);
@@ -216,10 +205,166 @@ public class BuscaminasGit extends JFrame implements ActionListener, MouseListen
         /*add(prueba);
         prueba.addActionListener(this);*/
     }//end constructor Mine()
- 
+    
+    public BuscaminasGit(int n, int m, int nomines, int [][] mines, int guesses[][], int perm[][]){
+        this.n = n;
+        this.m = m;
+        this.nomines = nomines;
+        nominesAux = nomines;
+        //new BuscaminasGit();
+        numeroMinas= new JLabel("Minas Restantes: ");
+        tiempoTranscurrido =new JLabel("Tiempo de juego: ");
+        menu = new JMenu("Menu");
+        Font font = new Font("Calibri", 3, 15);
+        menu.setFont(font);
+        //tiempoTranscurrido.setBackground(OPAQUE);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        minasRestantes = new JTextField(nomines);
+        minasRestantes.setEditable(false);
+        cronometro=new Cronometro();
+        barraMenu = new JMenuBar();
+        setJMenuBar(barraMenu);
+        menu.setBackground(Color.black);
+        barraMenu.add(menu);
+        mi1 = new JMenuItem("Reiniciar partida");
+        //mi1.addActionListener(this);
+        mi1.addActionListener((ActionEvent evento) -> {
+            this.setVisible(false);
+            new BuscaminasGit(n,m,nominesAux);
+            
+        });
+        menu.add(mi1);
+        mi1.addActionListener((ActionEvent evento) -> {
+            this.setVisible(false);
+            new BuscaminasGit(n,m,nominesAux);
+            
+        });
+        mi2 = new JMenuItem("Guardar partida");
+
+        
+        mi2.addActionListener((ActionEvent evento) -> {
+            //hilo.destroy();
+            cronometro.pararCronometro();
+
+            JFileChooser selectorFichero = new JFileChooser();
+            selectorFichero.setDialogTitle("Selecciona Fichero BackUp");
+            selectorFichero.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        
+            int resultado = selectorFichero.showSaveDialog(this);
+            if (resultado == JFileChooser.APPROVE_OPTION) {
+
+            boolean resultadoOK = this.hacerBackUp(selectorFichero.getSelectedFile().getAbsolutePath());
+            if (resultadoOK) {
+                JOptionPane.showMessageDialog(this, "Fichero guardado correctamente", "Guardar Fichero", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(this, "Fichero NO guardado", "Guardar Fichero", JOptionPane.ERROR_MESSAGE);
+            }
+        }  
+        });
+        menu.add(mi2);
+        mi3 = new JMenuItem("Cargar partida");
+        //mi3.addActionListener(this);
+        mi3.addActionListener((ActionEvent evento) -> {
+            JFileChooser selectorFichero = new JFileChooser();
+            selectorFichero.setDialogTitle("Selecciona Fichero BackUp");
+            selectorFichero.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        
+            int resultado = selectorFichero.showOpenDialog(this);
+            if (resultado == JFileChooser.APPROVE_OPTION) {
+                boolean resultadoOK = this.restaurarBackUp(selectorFichero.getSelectedFile().getAbsolutePath());
+                if (resultadoOK) {
+                    this.setVisible(false);
+                    JOptionPane.showMessageDialog(this, "Fichero cargado correctamente", "Cargar Fichero", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(this, "Fichero NO cargado", "Cargar Fichero", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        }
+            
+        );
+        menu.add(mi3);
+        menu.setMinimumSize(new Dimension(50,0));
+        barraMenu.setLayout(new BoxLayout(barraMenu, BoxLayout.X_AXIS));
+        cronometro.iniciarCronometro();
+        hilo=new Thread(cronometro);
+        hilo.start();
+        barraMenu.add(Box.createRigidArea(new Dimension(400,0)));  
+        barraMenu.add(tiempoTranscurrido);
+        cronometro.getTiempo().setOpaque(false);
+        barraMenu.add(cronometro.getTiempo());
+        barraMenu.add(Box.createRigidArea(new Dimension(450,0)));
+        barraMenu.add(numeroMinas);
+        barraMenu.add(minasRestantes);
+        //this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        perm = new int[n][m];
+        boolean allmines = false;
+        this.guesses = guesses;
+        this.mines = mines;
+        b = new JButton [n][m];
+        setLayout(new GridLayout(n,m));
+        /*for (int y = 0;y<m+2;y++){
+            mines[0][y] = 3;
+            mines[n+1][y] = 3;
+            guesses[0][y] = 3;
+            guesses[n+1][y] = 3;
+        }
+        for (int x = 0;x<n+2;x++){
+            mines[x][0] = 3;
+            mines[x][m+1] = 3;
+            guesses[x][0] = 3;
+            guesses[x][m+1] = 3;
+        } */
+        /*do {
+            int check = 0;
+            for (int y = 1;y<m+1;y++){
+                for (int x = 1;x<n+1;x++){
+                    mines[x][y] = 0;
+                    guesses[x][y] = 0;
+                }
+            }
+            for (int x = 0;x<nomines;x++){
+                mines [(int) (Math.random()*(n)+1)][(int) (Math.random()*(m)+1)] = 1;
+            }
+            for (int x = 0;x<n;x++){
+                for (int y = 0;y<m;y++){
+                if (mines[x+1][y+1] == 1){
+                        check++;
+                    }
+                }
+            }
+            if (check == nomines){
+                allmines = true;
+            }
+        }while (allmines == false); */
+        for (int y = 0;y<m;y++){
+            for (int x = 0;x<n;x++){
+                if ((mines[x+1][y+1] == 0) || (mines[x+1][y+1] == 1)){
+                    perm[x][y] = perimcheck(x,y);
+                }
+                b[x][y] = new JButton("?");
+                b[x][y].addActionListener(this);
+                b[x][y].addMouseListener(this);
+                add(b[x][y]);
+                b[x][y].setEnabled(true);
+                minasRestantes.setText(String.valueOf(nomines));
+            }//end inner for
+        }//end for
+        pack();
+        setVisible(true);
+        for (int y = 0;y<m+2;y++){
+            for (int x = 0;x<n+2;x++){
+                System.out.print(mines[x][y]);
+            }
+        System.out.println("");}
+        starttime = System.nanoTime();
+        
+        /*add(prueba);
+        prueba.addActionListener(this);*/
+    }//end constructor Mine()
     public void actionPerformed(ActionEvent e){
          
-        if(e.getClass().equals(JButton.class)){
+        
             found =  false;
             JButton current = (JButton)e.getSource();
             for (int y = 0;y<m;y++){
@@ -260,7 +405,7 @@ public class BuscaminasGit extends JFrame implements ActionListener, MouseListen
                     checkifend();
                 }
             }
-        }
+        
         
     }
  
@@ -376,7 +521,24 @@ public class BuscaminasGit extends JFrame implements ActionListener, MouseListen
             
             // Escritura
             //cronometro.pararCronometro();
-            oos.writeObject(this);
+            Datos datos = new Datos();
+            datos.setN(n);
+            datos.setM(m);
+            datos.setNomines(nomines);
+            datos.setMines(mines);
+            datos.setGuesses(guesses);
+            datos.setPerm(perm);
+            Botones botones[][]=datos.getBotones();
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    //datos.getBotones()[n][m].setColor(b[n][m].getBackground());
+                    botones[n][m].setText(b[n][m].getText());
+                    botones[n][m].setEstado(b[n][m].isEnabled());
+                }
+            }
+            datos.setBotones(botones);
+            
+            oos.writeObject(datos);
             // Cierre
             oos.flush();
             oos.close();
@@ -395,8 +557,9 @@ public class BuscaminasGit extends JFrame implements ActionListener, MouseListen
             ObjectInputStream ois = new ObjectInputStream(bis);
             
             // Lectura
-       
-            buscaminas = (BuscaminasGit) ois.readObject();
+            Datos datos = (Datos) ois.readObject();
+            BuscaminasGit buscaminas = new BuscaminasGit(datos.getN(), datos.getM(), datos.getNomines(), datos.getMines(), datos.getGuesses(),datos.getPerm());
+            
             // Cierre
             ois.close();
             return true;
